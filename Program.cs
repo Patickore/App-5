aeba3542eb5d70d00a336bff07379448624d5544
Patrick <patri@LAPTOP-Q3T1BCTA>
2022-07-30 18:06:45 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace App_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un numero");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese otro numero");
+            int num2 = int.Parse(Console.ReadLine());
+            var suma = num1 + num2;
+            Console.WriteLine("La suma de los numeros es: {0}", suma);
+            var resta = num1 - num2;
+            Console.WriteLine("La resta de los numeros es: {1}", resta);
+            var multiplicacion = num1 + num2;
+            Console.WriteLine("La multiplicacion de los numeros es: {2}", multiplicacion);
+            var division = num1 / num2;
+            Console.WriteLine("La division de los numeros es: {3}", division);
+        }
+    }
+}
